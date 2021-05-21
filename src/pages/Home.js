66ffdc61666c3b99b4fb11e6bea252ceea563a03
Marian <wyz_marian_013@yahoo.com>
2021-05-21 12:48:49 +0300

    update 31

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,14 +6,188 @@ import AuthenticationContext from "../AuthenticationContext";
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { Link } from "react-router-dom";
 
+function RegisterForm(props) {
+  const [passwordRequirements, setPasswordRequirements] = useState(false);
+
+  function checkPassword(str) {
+    if (str.length < 6) {
+        return("Password has less than 6 characters!");
+    } else if (str.length > 20) {
+        return("Password has more than 20 characters!");
+    } else if (str.search(/\d/) == -1) {
+        return("Password has no digit!");
+    } else if (str.search(/[a-z]/) == -1) {
+        return("PassWord has no lowercase letter!");
+    } else if (str.search(/[A-Z]/) == -1) {
+      return("PassWord has no capital letter!");
+    } else if (str.search(/[^a-zA-Z0-9\!\@\#\$\%\^\&\*\(\)\_\+]/) != -1) {
+        return("Password contains an invalid character!");
+    }
+    return("ok");
+  }
+
+  function checkInfo() {
+    if (checkPassword(props.passwordState) && props.passwordState == props.confirmPasswordState){
+      return true;
+    }
+    return false;
+  }
+
+  useEffect(() => {
+    props.setEmailState("");
+    props.setNameState("");
+    props.setPasswordState("");
+    props.setConfirmPasswordState("");
+  },[])
+
+  return (
+    <div>
+      {
+        props.registerSuccessful == 0 ?
+        <div className="myColumnSimple">
+          <label>Email</label>
+          <input 
+            type="text"
+            value={props.emailState}
+            onChange={(event) => {
+              props.setEmailState(event.target.value); 
+              if(props.emailValidationState == 1) {
+                props.setEmailValidationState(0);
+              }
+            }}
+          />
+          {props.emailValidationState == 1 ? <pre>An account is already registered with this email</pre> : <br/>}
+
+          <label>Name</label>
+          <input 
+            type="text"
+            value={props.nameState}
+            onChange={(event) => {
+              props.setNameState(event.target.value);
+              if(props.nameValidationState == 1) {
+                props.setNameValidationState(0);
+              }
+            }}
+          />
+          {props.nameValidationState == 1 ? <pre>An account is already registered with this name</pre> : <br/>}
+
+          <label>Password</label>
+          <input 
+            type="password"
+            value={props.passwordState}
+            onChange={(event) => props.setPasswordState(event.target.value)}
+            onFocus={(event) => setPasswordRequirements(true)}
+            onBlur={(event) => setPasswordRequirements(false)}
+          />
+          {
+            //props.passwordState != "" ?
+            (checkPassword(props.passwordState) != "ok" ?
+            <pre>{checkPassword(props.passwordState)}</pre> :
+            (
+              props.passwordState != props.confirmPasswordState ?
+              <pre>Password and confirmation must be equal</pre> :
+              <br/>
+            ))
+            //:<br/>
+          }
+          {
+            passwordRequirements ? 
+            <div>
+              <p>Password:</p>
+              <p>-must have between 6 and 20 characters</p>
+              <p>-must contain at least one digit</p>
+              <p>-must contain at least one lowercase letter</p>
+              <p>-must contain at least one capital letter</p>
+              <p>can contain one of the following symbols: !@#$%^&*()_+</p>
+            </div> :
+            <></>
+          }
+
+          <label>Confirm Password</label>
+          <input 
+            type="password"
+            value={props.confirmPasswordState}
+            onChange={(event) => props.setConfirmPasswordState(event.target.value)}
+          />
+          <br/>
+          <hr></hr>
+          <button onClick={checkInfo() ? props.handleRegister : null}>
+            Register
+          </button>
+        </div> :
+        <div>
+          Register successful
+        </div>
+      }
+    </div>
+  )
+}
+
+function LoginForm(props) {
+  useEffect(() => {
+    props.setEmailState("");
+    props.setPasswordState("");
+  },[])
+  return (
+    <div className="myColumnSimple">
+      <label>Email</label>
+      <input 
+        type="text"
+        value={props.emailState}
+        onChange={(event) => {
+          props.setEmailState(event.target.value); 
+          if(props.emailValidationState == 1) {
+            props.setEmailValidationState(0);
+          }
+        }}
+      />
+      {props.emailValidationState == 1 ? <pre>There is no account registered with this email</pre> : <br/>}
+    
+      <label>Password</label>
+      <input 
+        type="password"
+        value={props.passwordState}
+        onChange={(event) => {
+          props.setPasswordState(event.target.value);
+          if(props.passwordValidationState == 1) {
+            props.setPasswordValidationState(0);
+          }
+        }}
+      />
+      {props.passwordValidationState == 1 ? <pre>The password is incorrect</pre> : <br/>}
+
+      <br/>
+      <hr></hr>
+      <button onClick={props.handleLogin}>
+        Login
+      </button>
+      
+    </div>
+  )
+}
+
 export default function Home() {
   const history = useHistory();
   const authentication = useContext(AuthenticationContext);
-  let myId = authentication.getUser();
+  //let myId = authentication.getUser();
+  const [myId, setMyId] = useState(authentication.getUser());
 
   const [nameSearchState, setNameSearchState] = useState("");
   const [userIdState, setUserIdState] = useState("");
   const [postingListState, setPostingListState] = useState([]);
+
+  const [isLoginState, setIsLoginState] = useState(true);  
+
+  const [emailState, setEmailState] = useState("");
+  const [nameState, setNameState] = useState("");
+  const [passwordState, setPasswordState] = useState("");
+  const [confirmPasswordState, setConfirmPasswordState] = useState("");
+
+  const [emailValidationState, setEmailValidationState] = useState(0);
+  const [nameValidationState, setNameValidationState] = useState(0);
+  const [passwordValidationState, setPasswordValidationState] = useState(0);
+
+  const [registerSuccessful, setRegisterSuccessful] = useState(0);
 
   function handleChange(event){
     const { name, value } = event.target;
@@ -33,35 +207,6 @@ export default function Home() {
       });
   }
 
-  function handleChangeId(event){
-    const { name, value } = event.target;
-    setUserIdState(value);
-  }
-  function handleClickLogIn(){
-    authentication.logIn(userIdState);
-    myId = userIdState;
-    loadMorePostings();
-  }
-
-  function saveSeen(data) {
-    data.map((elem) => {
-      let URL = "/posting/" + elem.id;
-      let posting = {
-        "id": data.id,
-        "seenBy": [
-          {
-            "id": myId
-          }
-        ]
-      }
-      httpService
-        .put(URL,posting)
-        .then((response) => {
-          console.log(response);
-        })
-    })
-  }
-
   const loadMorePostings = () => 
     httpService
       .get("/profile/" + myId + "/unseen")
@@ -74,7 +219,7 @@ export default function Home() {
           postingListCopy.push(elem);
           
         });
-        console.log(postingListCopy);
+        //console.log(postingListCopy);
         //saveSeen(data);
         setPostingListState(postingListCopy);
       })
@@ -84,34 +229,163 @@ export default function Home() {
     history.push(path);
   }
 
-  useEffect(() => {
-    console.log(postingListState);
-    //saveSeen(postingListState.slice(-2))
-  },[postingListState])
+  function handleRegister() {
+    console.log("aqw");
+    let emailURL = "/account/exists/email/" + emailState;
+    let nameURL = "/profile/exists/name/" + nameState;
+    httpService
+      .get(emailURL)
+      .then((response) => {
+        console.log(response.data)
+        if (response.data) {
+          setEmailValidationState(1);
+        } else {
+          setEmailValidationState(2);
+        }
+      })
+      .catch((e) => {
+          console.log(e);
+        })
 
+      httpService
+      .get(nameURL)
+      .then((response) => {
+        console.log(response.data)
+        if (response.data) {
+          setNameValidationState(1);
+        } else {
+          setNameValidationState(2);
+        }
+      })
+      .catch((e) => {
+          console.log(e);
+        })
+  }
+
+  function handleLogin(){
+    let URL = "/account/login";
+    let account = {
+      "email": emailState,
+      "password": passwordState
+    }
+    console.log(account);
+    httpService
+      .post(URL, account)
+      .then((response) => {
+        console.log(response);
+        if (response.status == 200) {
+          setPasswordValidationState(0);
+          setEmailValidationState(0);
+          authentication.logIn(response.data);
+          console.log(response.data);
+          setMyId(response.data);
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+        let response = e.response;
+        if (response.status == 404) {
+          setEmailValidationState(1);
+        } else if (response.status == 403) {
+          setPasswordValidationState(1);
+        }
+      })
+  }
+
+  useEffect(() => {
+    if(emailValidationState == 2 && nameValidationState == 2) {
+      let URL = "/account";
+      let account = {
+        "account": {
+          "id": 0,
+          "email": emailState,
+          "password": passwordState
+        },
+        "name": nameState
+      }
+      httpService
+        .post(URL, account)
+        .then((response) => {
+          console.log(response.data);
+          setEmailValidationState(0);
+          setNameValidationState(0);
+          setRegisterSuccessful(1);
+          setEmailState("");
+          setNameState("");
+          setPasswordState("");
+          setConfirmPasswordState("");
+        })
+        .catch((e) => {
+          console.log(e);
+        })
+    }
+  },[emailValidationState,nameValidationState])
+
+  /*useEffect(() => {
+    if(myId!=-1){
+      loadMorePostings();
+    }
+  },[])*/
 
   useEffect(() => {
     if(myId!=-1){
       loadMorePostings();
     }
-  },[])
+  },[myId])
+
+  useEffect(() => {
+    console.log(registerSuccessful)
+  },[registerSuccessful])
 
   function HomePage(){
     if(myId==-1){
       return (
-        <div>
-          <input
-            type="text"
-            placeholder="Enter Id"
-            name="Id"
-            value={userIdState}
-            onChange={handleChangeId}
-          />
-          <button
-            onClick={handleClickLogIn}
-          >
-            LogIn
-          </button>
+        <div className="myColumnSimple">
+          <h3>{isLoginState ? "Login" : "Register"}</h3>
+          <div className="myRowSimple">
+            <button onClick={event => !isLoginState ? setIsLoginState(true) : null}>
+              Login
+            </button>
+            <button onClick={event => {
+                if (isLoginState) {
+                  setIsLoginState(false)
+                }
+                setRegisterSuccessful(0);
+              }}>
+              Register
+            </button>
+          </div>
+          <hr/>
+          {
+            isLoginState ? 
+            <LoginForm 
+              emailState={emailState}
+              setEmailState={setEmailState}
+              emailValidationState={emailValidationState}
+              setEmailValidationState={setEmailValidationState}
+              passwordState={passwordState}
+              setPasswordState={setPasswordState}
+              passwordValidationState={passwordValidationState}
+              setPasswordValidationState={setPasswordValidationState}
+              handleLogin={handleLogin}
+            /> :
+            <RegisterForm 
+              emailState={emailState}
+              setEmailState={setEmailState}
+              emailValidationState={emailValidationState}
+              setEmailValidationState={setEmailValidationState}
+              nameState={nameState}
+              setNameState={setNameState}
+              nameValidationState={nameValidationState}
+              setNameValidationState={setNameValidationState}
+              passwordState={passwordState}
+              setPasswordState={setPasswordState}
+              confirmPasswordState={confirmPasswordState}
+              setConfirmPasswordState={setConfirmPasswordState}
+              handleRegister={handleRegister}
+              registerSuccessful={registerSuccessful}
+            />
+          }
         </div>
       )
     } else {
